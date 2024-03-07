e308601778e65677f7c193c5c2501af80c3c98fd
Olga Gorobets <dorofeeva.olga74@mail.ru>
2024-03-08 02:43:51 +0900

    Небольшие изменения

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api.jupiter.nomoredomainsmonster.ru'; 
+const BASE_URL = 'https://api.jupiter.movies.nomoredomainsmonster.ru'; 
 // const BASE_URL = 'http://localhost:3000';
 
 // Приватный метод ответа сервера
