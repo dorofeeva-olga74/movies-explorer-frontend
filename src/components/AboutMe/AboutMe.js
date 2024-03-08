5198231c9c8c68d7e7abfadc2ce15d8c89fb81fe
Olga Gorobets <dorofeeva.olga74@mail.ru>
2024-03-08 13:56:47 +0900

    add new photo

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import photo from '../../images/photoMe.svg';
+import photo from '../../images/photoMe.jpeg';
 import Portfolio from '../Portfolio/Portfolio';
 
 function AboutMe() {
